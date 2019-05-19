05ba42aa781a2c748ecf3db826a01ce5759d2f0d
Adelina Enache <enache.adelina99@gmail.com>
2019-05-19 10:57:13 +0300

    use cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const { questionRoutes }  = require('./routes');
 
@@ -12,8 +13,11 @@ mongoose.connect("mongodb://user:user123@ds159036.mlab.com:59036/euthing", { use
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+
 app.use('/question', questionRoutes);
-const port = process.env.PORT || 3000;
+
+const port = 8000;
 
 app.listen(port, () => { 
     console.log(`Serving on ${port}`);
